dd124527db563e091d811735b44d07d48f888b28
Masso <masso-dev@outlook.fr>
2017-07-29 20:51:03 +0200

    First implementation of Purge Object

--- commands/Purge.js
+++ commands/Purge.js
@@ -4,19 +4,33 @@ module.exports = class Purge
     constructor(message, params)
     {
         this.message = message
-        this.params = params[0]
+        this.mention = params[0]
+
+        if (params[1])
+            this.limit = params[1]
+        else
+            this.limit = 100
+        
         this.action()
     }
 
     action()
     {
         let regex = /<@([0-9]+)>/
-        let params = regex.exec(this.params)[1]
+        let params = regex.exec(this.mention)[1]
         let member = this.message.guild.members.get(params)
 
         if (member.user.id == params)
         {
-            console.log(this.message.channel)
+            this.message.channel.fetchMessages({limit: this.limit})
+            .then(messages => {
+                messages = messages.array()
+                for (let i = 0; i < messages.length; i++)
+                {
+                    if (messages[i].author.id == params)
+                        messages[i].delete()
+                }
+            })
         }
     }
 
